c79a9715a9918cb86d0d9922aaa11ac40117e1c8
Abishek-sk46 <skabishekskabishek3395@gmail.com>
2025-02-10 10:37:59 +0530

    Saving changes

--- blog/forms.py
+++ blog/forms.py
@@ -27,42 +27,42 @@ class RegisterForm(forms.ModelForm):
         if password and password_confirm and password != password_confirm:
             raise forms.ValidationError("Passwords do not match.")
 
-# class LoginForm(forms.Form):
-#     username = forms.CharField(label='username', max_length=100, required=True)
-#     password = forms.CharField(label='password', max_length=100, required=True)
+class LoginForm(forms.Form):
+    username = forms.CharField(label='username', max_length=100, required=True)
+    password = forms.CharField(label='password', max_length=100, required=True)
 
-#     def clean(self):
-#         cleaned_data = super().clean()
-#         username = cleaned_data.get("username")
-#         password = cleaned_data.get("password")
-#         if username and password:
-#             user = authenticate(username=username, password=password)
-#             if user is None:
-#                 raise forms.ValidationError("Invalid username or password")
+    def clean(self):
+        cleaned_data = super().clean()
+        username = cleaned_data.get("username")
+        password = cleaned_data.get("password")
+        if username and password:
+            user = authenticate(username=username, password=password)
+            if user is None:
+                raise forms.ValidationError("Invalid username or password")
 
-# class ForgotPasswordForm(forms.Form):
-#     email = forms.EmailField(label='Email', max_length=254, required=True)
+class ForgotPasswordForm(forms.Form):
+    email = forms.EmailField(label='Email', max_length=254, required=True)
 
-#     def clean(self):
-#         cleaned_data = super().clean()
-#         email = cleaned_data.get('email')
+    def clean(self):
+        cleaned_data = super().clean()
+        email = cleaned_data.get('email')
 
-#         if not User.objects.filter(email=email).exists():
-#             raise forms.ValidationError("No User Registered with this email.")
+        if not User.objects.filter(email=email).exists():
+            raise forms.ValidationError("No User Registered with this email.")
         
-# class ResetPasswordForm(forms.Form):
-#     new_password = forms.CharField(label='New Password', min_length=8)
-#     confirm_password = forms.CharField(label='Confirm Password', min_length=8)
+class ResetPasswordForm(forms.Form):
+    new_password = forms.CharField(label='New Password', min_length=8)
+    confirm_password = forms.CharField(label='Confirm Password', min_length=8)
 
-#     def clean(self):
-#         cleaned_data = super().clean()
-#         new_password = cleaned_data.get("new_password")
-#         confirm_password = cleaned_data.get("confirm_password")
+    def clean(self):
+        cleaned_data = super().clean()
+        new_password = cleaned_data.get("new_password")
+        confirm_password = cleaned_data.get("confirm_password")
 
-#         if new_password and confirm_password and new_password != confirm_password:
-#             raise forms.ValidationError("Passwords do not match.")
+        if new_password and confirm_password and new_password != confirm_password:
+            raise forms.ValidationError("Passwords do not match.")
 
-# class PostForm(forms.ModelForm):
+class PostForm(forms.ModelForm):
     title = forms.CharField(label='Title', max_length=200, required=True)
     content = forms.CharField(label='Content', required=True)
     category =  forms.ModelChoiceField(label='Category', required=True, queryset=Category.objects.all())
